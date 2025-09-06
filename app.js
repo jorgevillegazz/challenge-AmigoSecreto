2a4bd3ff78f235624aa9c246e90eee67958c57b6
JorgeVillegazzz <jorgeavc79@gmail.com>
2025-09-06 01:06:57 -0600

    cambios finales  fuincionales

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let ListadeAmigos = []; //arreglo de los  nombres
+let conteo = 0;        // su nombre  lo dice
+
+console.log(conteo);
+console.log (ListadeAmigos);
+
+function agregarAmigo(){
+
+    let nombreAmigo = document.getElementById('amigo').value;
+    if(nombreAmigo== null){
+      return ;
+    }
+    else{
+    ListadeAmigos.push(nombreAmigo);                //almacenando el nombre en la lista
+    console.log (ListadeAmigos);
+    conteo++;
+   asignarTextoElemento('ul',`Amigo número ${conteo} ${nombreAmigo}`);//muestra debajo de la caja de texto  el numero y  nombre asigando 
+   limpiarcaja();
+    }
+
+}
+
+function limpiarcaja(){
+  let valorcaja= document.getElementById('amigo');
+  valorcaja.value='';
+}
+
+function sortearAmigo()
+{
+  let num = Math.floor(Math.random()*3)+1;    //Generamos un numero aleatorio para sortear el nombre del amigo secreto
+  console.log(num);
+  num --;
+  console.log(ListadeAmigos[num]);   // Muestra el nombre del amigo 
+  let amigosecreto = ListadeAmigos[num];
+  asignarTextoElemento('h2',`El Amigo(a) secreto es: ${amigosecreto}`);// cambia el texto del encabezado2 con el nombre sorteado
+
+}
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
